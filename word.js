9e06c127c7b508dffa5229376cb7c9336b13bf43
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-07-04 07:40:28 +0200

    add more words to list

--- word.js
+++ word.js
@@ -9,10 +9,10 @@ const wordList = [
     },
     {
         word: "aim",
-        hint: "purpose or intention"
+        hint: "a purpose or intention"
     },
     {
-        word: "saturn",
+        word: "venus",
         hint: "planet of our solar system"
     },
     {
@@ -20,115 +20,179 @@ const wordList = [
         hint: "a yellow precious metal"
     },
     {
+        word: "ebay",
+        hint: "online shopping site"
+    },
+    {
         word: "golang",
         hint: "programming language"
     },
     {
-        word: "idea",
-        hint: "a thought or suggestion"
+        word: "coding",
+        hint: "related to programming"
     },
     {
-        word: "svg",
-        hint: "a vector image format"
+        word: "matrix",
+        hint: "science fiction movie"
     },
     {
-        word: "server",
-        hint: "related to computer or system"
+        word: "bugs",
+        hint: "related to programming"
     },
     {
-        word: "jpeg",
-        hint: "a small image file format"
+        word: "avatar",
+        hint: "epic science fiction film"
     },
     {
-        word: "search",
-        hint: "act to find something"
+        word: "gif",
+        hint: "a file format for image"
     },
     {
-        word: "air",
-        hint: "the invisible gaseous"
+        word: "mental",
+        hint: "related to the mind"
     },
     {
-        word: "html",
-        hint: "markup language for the web"
-    },
-    {
-        word: "email",
-        hint: "related to exchanging message"
-    },
-    {
-        word: "mars",
-        hint: "second smallest planet"
-    },
-    {
-        word: "apple",
-        hint: "mobile manufacture brand"
-    },
-    {
-        word: "rocket",
-        hint: "space vehicle or missile"
-    },
-    {
-        word: "tesla",
-        hint: "unit of magnetic flux destiny"
-    },
-    {
-        word: "crypto",
-        hint: "related to cryptocurrency"
-    },
-    {
-        word: "flute",
-        hint: "a musical insturment"
-    },
-    {
-        word: "banana",
-        hint: "yellow long curved fruit"
-    },
-    {
-        word: "google",
-        hint: "famous search engine"
-    },
-    {
-        word: "java",
-        hint: "programming language"
-    },
-    {
-        word: "silver",
-        hint: "precious greyish-white metal"
-    },
-    {
-        word: "png",
-        hint: "type of raster image file"
-    },
-    {
-        word: "github",
-        hint: "code hosting platform "
-    },
-    {
-        word: "chess",
-        hint: "game played by two player"
-    },
-    {
-        word: "act",
-        hint: "the doing of a thing"
-    },
-    {
-        word: "hockey",
-        hint: "a famous outdoor game"
+        word: "map",
+        hint: "diagram represent of an area"
     },
     {
         word: "island",
         hint: "land surrounded by water"
     },
     {
-        word: "map",
-        hint: "diagram repesentation of area"
+        word: "hockey",
+        hint: "a famous outdoor game"
     },
     {
-        word: "mental",
-        hint: "realating to the mind"
+        word: "chess",
+        hint: "related to an indoor game"
     },
     {
-        word: "joking",
-        hint: "humorous or flippant"
+        word: "viber",
+        hint: "a social media app"
+    },
+    {
+        word: "github",
+        hint: "code hosting platform"
+    },
+    {
+        word: "png",
+        hint: "a image file format"
+    },
+    {
+        word: "silver",
+        hint: "precious greyish-white metal"
+    },
+    {
+        word: "mobile",
+        hint: "an electronic device"
+    },
+    {
+        word: "gpu",
+        hint: "computer component"
+    },
+    {
+        word: "java",
+        hint: "programming language"
+    },
+    {
+        word: "google",
+        hint: "famous search engine"
+    },
+    {
+        word: "venice",
+        hint: "famous city of waters"
+    },
+    {
+        word: "excel",
+        hint: "microsoft product for windows"
+    },
+    {
+        word: "mysql",
+        hint: "a relational database system"
+    },
+    {
+        word: "nepal",
+        hint: "developing country name"
+    },
+    {
+        word: "flute",
+        hint: "a musical instrument"
+    },
+    {
+        word: "crypto",
+        hint: "related to cryptocurrency"
+    },
+    {
+        word: "tesla",
+        hint: "unit of magnetic flux density"
+    },
+    {
+        word: "mars",
+        hint: "planet of our solar system"
+    },
+    {
+        word: "proxy",
+        hint: "related to server application"
+    },
+    {
+        word: "email",
+        hint: "related to exchanging message"
+    },
+    {
+        word: "html",
+        hint: "markup language for the web"
+    },
+    {
+        word: "air",
+        hint: "related to a gas"
+    },
+    {
+        word: "idea",
+        hint: "a thought or suggestion"
+    },
+    {
+        word: "server",
+        hint: "related to computer or system"
+    },
+    {
+        word: "svg",
+        hint: "a vector image format"
+    },
+    {
+        word: "jpeg",
+        hint: "a image file format"
+    },
+    {
+        word: "search",
+        hint: "act to find something"
+    },
+    {
+        word: "key",
+        hint: "small piece of metal"
+    },
+    {
+        word: "egypt",
+        hint: "a country name"
+    },
+    {
+        word: "joker",
+        hint: "psychological thriller film"
+    },
+    {
+        word: "dubai",
+        hint: "developed country name"
+    },
+    {
+        word: "photo",
+        hint: "representation of person or scene"
+    },
+    {
+        word: "nile",
+        hint: "largest river in the world"
+    },
+    {
+        word: "rain",
+        hint: "related to a water"
     },
 ]
