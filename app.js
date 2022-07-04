4988e0dede2c2e3c397b4aa8afcded659b597b1a
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-07-04 06:50:43 +0200

    get the amount of inputs from the length of the word

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
+const inputs = document.querySelector('.inputs')
+
 function randomWord() {
-    let randomObjectWord = wordList[Math.floor(Math.random() * wordList.length)]
-    console.log(randomObjectWord)
+    let randomObject = wordList[Math.floor(Math.random() * wordList.length)]
+    let word = randomObject.word;
+    console.log(word)
+
+    let html = ""
+    for (let i = 0; i < word.length; i++) {
+        html += `<input type="text" disabled>`
+    }
+    inputs.innerHTML = html;
+
+
 }
 
 randomWord()
