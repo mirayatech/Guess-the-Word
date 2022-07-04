1ac3dc7f170314f5638e8f01cdec86229ac21b08
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-07-04 06:32:31 +0200

    create the words and hints

---
+++ word.js
@@ -0,0 +1,134 @@
+const wordList = [
+    {
+        word: "python",
+        hint: "programming language"
+    },
+    {
+        word: "guitar",
+        hint: "a musical instrument"
+    },
+    {
+        word: "purpose or intention",
+        hint: "aim"
+    },
+    {
+        word: "saturn",
+        hint: "planet of our solar system"
+    },
+    {
+        word: "gold",
+        hint: "a yellow precious metal"
+    },
+    {
+        word: "golang",
+        hint: "programming language"
+    },
+    {
+        word: "idea",
+        hint: "a thought or suggestion"
+    },
+    {
+        word: "svg",
+        hint: "a vector image format"
+    },
+    {
+        word: "server",
+        hint: "related to computer or system"
+    },
+    {
+        word: "jpeg",
+        hint: "a small image file format"
+    },
+    {
+        word: "search",
+        hint: "act to find something"
+    },
+    {
+        word: "air",
+        hint: "the invisible gaseous"
+    },
+    {
+        word: "html",
+        hint: "markup language for the web"
+    },
+    {
+        word: "email",
+        hint: "related to exchanging message"
+    },
+    {
+        word: "mars",
+        hint: "second smallest planet"
+    },
+    {
+        word: "apple",
+        hint: "mobile manufacture brand"
+    },
+    {
+        word: "rocket",
+        hint: "space vehicle or missile"
+    },
+    {
+        word: "tesla",
+        hint: "unit of magnetic flux destiny"
+    },
+    {
+        word: "crypto",
+        hint: "related to cryptocurrency"
+    },
+    {
+        word: "flute",
+        hint: "a musical insturment"
+    },
+    {
+        word: "banana",
+        hint: "yellow long curved fruit"
+    },
+    {
+        word: "google",
+        hint: "famous search engine"
+    },
+    {
+        word: "java",
+        hint: "programming language"
+    },
+    {
+        word: "silver",
+        hint: "precious greyish-white metal"
+    },
+    {
+        word: "png",
+        hint: "type of raster image file"
+    },
+    {
+        word: "github",
+        hint: "code hosting platform "
+    },
+    {
+        word: "chess",
+        hint: "game played by two player"
+    },
+    {
+        word: "act",
+        hint: "the doing of a thing"
+    },
+    {
+        word: "hockey",
+        hint: "a famous outdoor game"
+    },
+    {
+        word: "island",
+        hint: "land surrounded by water"
+    },
+    {
+        word: "map",
+        hint: "diagram repesentation of area"
+    },
+    {
+        word: "mental",
+        hint: "realating to the mind"
+    },
+    {
+        word: "joking",
+        hint: "humorous or flippant"
+    },
+]
